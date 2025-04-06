31474c52a5f86652d35119a3328e49e966fb36a9
Diego Armando <dgox16@gmail.com>
2025-04-06 10:08:52 -0600

    feat(nvim.markdown): Now I use render-markdown

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -15,7 +15,9 @@ return {
     },
 
     {
-        "OXY2DEV/markview.nvim",
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+        opts = {},
         ft = "markdown",
     },
 }

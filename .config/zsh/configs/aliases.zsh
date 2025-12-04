c653d6b83ad32a0019f723d527033f487fc36dff
Diego Armando <dgox16@gmail.com>
2025-12-03 22:37:57 -0600

    feat(zsh): Add aliases for docker

--- .config/zsh/configs/aliases.zsh
+++ .config/zsh/configs/aliases.zsh
@@ -14,14 +14,31 @@ alias las="eza -a --color=always --icons --group-directories-first"
 alias lt="eza -T --color=always --icons --group-directories-first"
 alias lat="eza -aT --color=always --icons --group-directories-first"
 alias du="dust -b"
-
 alias glog='git log --graph --decorate --oneline'
 
-# Django
-alias djrun='python manage.py runserver'
-alias djmkm='python manage.py makemigrations'
-alias djmig='python manage.py migrate'
 
-# Start services
-alias spsql='sudo systemctl start postgresql.service'
-alias sdocker='sudo systemctl start docker'
+# Atajos básicos de docker
+alias d="docker"
+alias dps="docker ps"
+alias dpsa="docker ps -a"
+alias di="docker images"
+alias db="docker build"
+alias dex="docker exec -it"
+alias drm="docker rm"
+alias drmi="docker rmi"
+alias dst="docker stop"
+alias drs="docker restart"
+
+alias dc="docker compose"
+alias dcu="docker compose up -d"
+alias dcd="docker compose down"
+alias dcl="docker compose logs -f"
+alias dcb="docker compose build"
+alias dce="docker compose exec"
+
+alias dv="docker compose -f docker-compose.dev.yml"
+alias dvu="docker compose -f docker-compose.dev.yml up -d"
+alias dvd="docker compose -f docker-compose.dev.yml down"
+alias dvl="docker compose -f docker-compose.dev.yml logs -f"
+alias dvb="docker compose -f docker-compose.dev.yml build"
+alias dve="docker compose -f docker-compose.dev.yml exec"
